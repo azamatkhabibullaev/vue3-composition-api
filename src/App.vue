5966b316948b3c07e7d24a94ccaed6c967ead9ca
Azamat <a3amatkhabibullaev@gmail.com>
2022-07-07 00:23:58 +0500

    List rendering

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,27 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+
+const items = ref([
+  {id: 1, label: '10 party hats'},
+  {id: 2, label: '20 cups'},
+  {id: 3, label: '10 gaming chairs'},
+  {id: 3, label: '1 Awesome Vue Course'}
+])
 </script>
 
 <template>
   <h1>
     {{ header }}
   </h1>
+  <ul>
+    <li
+      v-for="({id, label}, index) in items"
+      :key="id"
+    >
+      {{ index + 1 }} - {{ label }}
+    </li>
+  </ul>
 </template>
 
 <style>
