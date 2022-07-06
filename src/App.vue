fc0384fcf247a36081f981d264679a3c6b0e4ad0
Azamat <a3amatkhabibullaev@gmail.com>
2022-07-07 01:57:48 +0500

    User input multiple checkbox

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ const items = ref([
 
 const newItem = ref('')
 const newItemHighPriority = ref(false)
+const iceCreamFlavors = ref([])
 </script>
 
 <template>
@@ -22,12 +23,29 @@ const newItemHighPriority = ref(false)
     <input v-model="newItem" type="text" placeholder="Add an item">
   </div>
 
-  <div>
+  <div class="input-group">
     <input type="checkbox" id="priority" v-model="newItemHighPriority">
     <label for="priority">High Priority</label>
   </div>
 
-  {{ newItemHighPriority }}
+  <div>{{ newItemHighPriority }}</div>
+
+  <div class="input-group">
+    <div>
+      <input type="checkbox" id="vanilla" v-model="iceCreamFlavors" value="vanilla">
+      <label for="vanilla">Vanilla</label>
+    </div>
+    <div>
+      <input type="checkbox" id="chocolate" v-model="iceCreamFlavors" value="chocolate">
+      <label for="chocolate">Chocolate</label>
+    </div>
+    <div>
+      <input type="checkbox" id="strawberry" v-model="iceCreamFlavors" value="strawberry">
+      <label for="strawberry">Strawberry</label>
+    </div>
+  </div>
+  
+  <div>{{ iceCreamFlavors }}</div>
 
   <ul>
     <li v-for="({ id, label }, index) in items" :key="id">
