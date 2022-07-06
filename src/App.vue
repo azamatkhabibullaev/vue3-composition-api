5af3a2a353ab66f3b65fc6a502b4badbfa8258d0
Azamat <a3amatkhabibullaev@gmail.com>
2022-07-07 01:35:02 +0500

    User input

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,25 @@ import { ref } from 'vue'
 const header = ref('Shopping List App')
 
 const items = ref([
-  {id: 1, label: '10 party hats'},
-  {id: 2, label: '20 cups'},
-  {id: 3, label: '10 gaming chairs'},
-  {id: 3, label: '1 Awesome Vue Course'}
+  { id: 1, label: '10 party hats' },
+  { id: 2, label: '20 cups' },
+  { id: 3, label: '10 gaming chairs' },
+  { id: 3, label: '1 Awesome Vue Course' }
 ])
+
+const newItem = ref('')
 </script>
 
 <template>
   <h1>
     {{ header }}
   </h1>
+  <input v-model.lazy="newItem" type="text" placeholder="Add an item">
+  <!-- lazy, trim, number, enter -->
+  <br>
+  {{ newItem }}
   <ul>
-    <li
-      v-for="({id, label}, index) in items"
-      :key="id"
-    >
+    <li v-for="({ id, label }, index) in items" :key="id">
       {{ index + 1 }} - {{ label }}
     </li>
   </ul>
