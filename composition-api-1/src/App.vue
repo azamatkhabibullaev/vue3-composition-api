3f545d0317a4e2bbe46559f039b99c20c9daf2af
Azamat <a3amatkhabibullaev@gmail.com>
2022-07-06 23:58:00 +0500

    Initial commit

---
+++ composition-api-1/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1>
+    {{ msg.toUpperCase() }}
+    <!-- {{ msg.toUpperCase(); msg.toLowerCase() }} -->
+    <!-- {{ const sum = 42; }} -->
+    <!-- {{ if(msg) { return 'Welcome!' } }} -->
+    {{ msg ? msg : 'Welcome!' }}
+    {{ msg || 'Welcome!' }}
+  </h1>
+  <input type="text" v-model="msg">
+  <!-- <input type="text" v-model="msg.toUpperCase()"> you cannot use .toUpperCase() inside v-model -->
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
